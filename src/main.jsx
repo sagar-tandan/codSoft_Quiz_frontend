796a2c3407af19e495bf3ad204c3a0d74b2aa637
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-07 16:07:17 +0545

    minor adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "antd/dist/antd.min.css";
+// import 'antd/dist/antd.min.css';
 import { Provider } from "react-redux";
-import store from "./redux/store";
+// import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
