bb7a81a60dec2fbbf8a3351bd3f1a180bfd0884c
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-07 16:03:10 +0545

    all previous css implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import "./stylesheets/alignments.css";
+import "./stylesheets/textelements.css";
+import "./stylesheets/theme.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/form-element.css";
+import "./stylesheets/layout.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { isTokenExp } from "./Utils/jwt";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading } = useSelector((state) => state.loaders);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const token = localStorage.getItem("token");
+  if (isTokenExp(token)) {
+    localStorage.removeItem("token");
+  }
+
+  return <></>;
 }
 
-export default App
+export default App;
