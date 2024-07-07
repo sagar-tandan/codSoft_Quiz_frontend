48fadfdde139739db982d81d891f8dbe3fdb1b88
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-07 16:12:09 +0545

    react redux work on progress

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import 'antd/dist/antd.min.css';
 import { Provider } from "react-redux";
-// import store from "./redux/store";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
